23d5f3bd7e00e5b720bd1b6becce82554e24b0cb
Natrix111 <dmitryk414@gmail.com>
2024-11-07 17:36:34 +0300

    logout

--- applications/urls.py
+++ applications/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- applications/views.py
+++ applications/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .forms import RegisterForm
 from django.urls import reverse_lazy
+from django.contrib.auth import logout
 
 def index(request):
     return render(request, 'index.html')
@@ -10,3 +11,7 @@ class Register(generic.CreateView):
     template_name = 'registration/register.html'
     form_class = RegisterForm
     success_url = reverse_lazy('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
