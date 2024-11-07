b3cb8f788e0c24e7e21f6db9bcfd9cfcd6c15cd3
Дмитрий Ковалев <gr422_kodpa@lab-402-5.tomtit-tomsk.ru>
2024-11-07 11:11:43 +0700

    registration

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, verbose_name='Имя', blank=False)
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия', blank=False)
+    patronymic = models.CharField(max_length=50, verbose_name='Отчество', blank=False)
+    username = models.CharField(max_length=200, verbose_name='Логин', unique=True, blank=False)
+    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
+    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.first_name
--- applications/urls.py
+++ applications/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.Register.as_view(), name='register'),
 ]
